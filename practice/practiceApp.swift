e7b61e0c5437e1a28ea619c535b0335285d03508
anthonymarie22 <72616953+anthonymarie22@users.noreply.github.com>
2023-02-01 13:41:48 -0500

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Anthony Marie on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
